56e77eefaa7999e746b10331e01704c0da4b96d9
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-25 22:10:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,8 @@ def monopoly_with_second_tier
 
 end
 
+
+
 def monopoly_with_third_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
